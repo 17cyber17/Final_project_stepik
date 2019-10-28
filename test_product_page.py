7466781a690c66d52c397b236aab590a2b33ecea
Semyon Kondratkov <17cyber17@gmail.com>
2019-10-28 12:36:21 +0700

    Добавленны недостающие проверки объектов на странице продукта

--- test_product_page.py
+++ test_product_page.py
@@ -4,6 +4,8 @@ def test_guest_can_add_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
     page = ProductPage(browser, link)
     page.open()
-    page.should_be_product_page()
+    page.should_be_add_to_basket_btn()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
+    page.should_be_message_about_adding()
+    page.should_be_message_basket_total()
